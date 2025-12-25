59294ab994619d6f5f1ffa9bec6f63dcdb7220fc
brandonviaje <brandon.viaje@ontariotechu.net>
2025-12-25 15:54:13 -0500

    fix: fix data preprocessing step

--- nnue_training/chunk_processing.py
+++ nnue_training/chunk_processing.py
@@ -10,35 +10,40 @@ os.makedirs(output_folder, exist_ok=True)
 # Process a chunk of data 
 def process_chunk(df_chunk: pd.DataFrame) -> tuple[np.ndarray, np.ndarray]:
     
-    X_list, y_list = [], []
+    X_white_list, X_black_list, y_list = [], [], []
 
     for fen, eval_str in zip(df_chunk['FEN'], df_chunk['Evaluation']):
         try:
-            # preprocess each FEN and evaluation
-            X, y = pre_processing.preprocess_fen_eval(fen, eval_str)
+            # preprocess each FEN and evaluation for both perspectives
+            x_white, y = pre_processing.preprocess_fen_eval(fen, eval_str)
+            x_black, _ = pre_processing.preprocess_fen_eval(fen, eval_str, perspective='black')
 
             # only keep non-mate positions
-            if X is not None:  
-                X_list.append(X)
+            if y is not None:  
+                X_white_list.append(x_white)
+                X_black_list.append(x_black)
                 y_list.append(y)
-            else:   
-                # skip mate positions
-                continue
         
-        # catch 
+        # catch errors in preprocessing
         except Exception as e:
             print("Skipped row due to error:", e)       
 
-    return np.array(X_list, dtype=np.uint8), np.array(y_list, dtype=np.float32)
+    return (
+    np.array(X_white_list, dtype=np.uint8),
+    np.array(X_black_list, dtype=np.uint8),
+    np.array(y_list, dtype=np.float32)
+    )
 
+# Read and process CSV in chunks
 chunksize = 100000
 
 for i, chunk in enumerate(pd.read_csv("chessData.csv", chunksize=chunksize)):
 
-    X_chunk, y_chunk = process_chunk(chunk)
+    X_white_chunk, X_black_chunk, y_chunk = process_chunk(chunk)
     file_path = os.path.join(output_folder, f"chunk_{i:03d}.h5")
 
     # Save each chunk as H5
     with h5py.File(file_path, "w") as f:
-        f.create_dataset("X", data=X_chunk, compression="gzip")
+        f.create_dataset("X_white", data=X_white_chunk, compression="gzip")
+        f.create_dataset("X_black", data=X_black_chunk, compression="gzip")
         f.create_dataset("y", data=y_chunk, compression="gzip")
